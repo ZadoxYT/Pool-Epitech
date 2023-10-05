42501ca6d87af2d2b05451435257abfa676fb36c
Zadox <33782530+ZadoxYT@users.noreply.github.com>
2023-10-05 15:31:59 +0200

    erratum

--- Day 5/Source/ex_03/ex_03.js
+++ Day 5/Source/ex_03/ex_03.js
@@ -1,10 +1,3 @@
-var paragraphs = document.querySelectorAll('p');
-var lastParagraph = paragraphs[paragraphs.length - 1];
-var newParagraph = document.createElement('p');
-
-newParagraph.textContent = 'Now featuring a headphone jack!';
-lastParagraph.insertAdjacentElement('afterend', newParagraph);
-
 document.addEventListener('DOMContentLoaded', function() {
     var paragraphs = document.querySelectorAll('p');
 var lastParagraph = paragraphs[paragraphs.length - 1];
