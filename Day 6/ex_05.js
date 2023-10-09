e593374f8924711a4b2b8ac9064820c7113c0894
Zadox <33782530+ZadoxYT@users.noreply.github.com>
2023-10-09 15:42:55 +0200

    finish

---
+++ Day 6/ex_05.js
@@ -0,0 +1,26 @@
+// Car object
+const car = {
+    color: "grey",
+    brand: "Aston Martin",
+    model: "DB5",
+    logInfo() {
+      console.log(`Car information: ${this.color}, ${this.model}, ${this.brand})`);
+    }
+  };
+  
+  // Agent object
+  const agent = {
+    firstname: "James",
+    lastname: "Bond",
+    code: '007',
+    age: 57,
+    car: car,
+    logInfo() {
+      console.log(`Agent information: ${this.firstname} ${this.lastname}, ${this.code}, ${this.age}`);
+      console.log(`James bond's Car information: ${car.color}, ${car.model}, ${car.brand}`);
+    }
+  };
+
+  agent.logInfo();
+  car.logInfo();
+  
